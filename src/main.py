83b69e5446ec2ed5e574cd529f0071a9c6d90a58
Max Ryoo <hyunsukr@gmail.com>
2021-11-06 02:37:10 -0400

    add continents

--- src/main.py
+++ src/main.py
@@ -1,27 +1,26 @@
 from utils.webscrapper import Web_Scrapper
 from utils.cleaner import Cleaner
 import pandas as pd
-import json
 
 def main():
     scrapper = Web_Scrapper()
     national_src = scrapper.scrape_summary("medal-standings.htm")
-    national_src.to_csv('country.csv')
+    national_src.to_csv('data/country.csv')
     gdp = scrapper.scrape_gdp()
-    gdp.to_csv("gdp.csv")
-    # Opening JSON Mapping file
-    with open('mapping.json') as json_file:
-        data = json.load(json_file)
-    
-        country_mapping = data
+    gdp.to_csv("data/gdp.csv")
 
-    mapper_class = Cleaner(data)
+    mapper_class = Cleaner()
     joined_df = mapper_class.join_gdp(gdp, national_src)
-    joined_df.to_csv("country_medal_gdp.csv")
+    joined_df.to_csv("data/country_medal_gdp.csv")
 
-    teams = pd.read_excel("Teams.xlsx")
-    final = mapper_class.join_aggregate_teams(teams, joined_df)
-    final.to_csv("teams_country_gdp.csv")
+    teams = pd.read_excel("src/resources/Teams.xlsx")
+    maps = mapper_class.join_aggregate_teams(teams, joined_df)
+    maps.to_csv("data/teams_country_gdp.csv")
+
+    continent_addition = mapper_class.convert_continent(maps)
+
+    continent_addition.to_csv("data/final_olympic_cont.csv")
+    print("Finished Data Processing")
 
 
 main()
--- src/utils/cleaner.py
+++ src/utils/cleaner.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import pycountry_convert as pc
+import json
+
 
 class Cleaner():
-    def __init__(self, mapping):
+    def __init__(self):
+        # Opening JSON Mapping file
+        with open('src/resources/mapping.json') as json_file:
+            mapping = json.load(json_file)
+
+        with open('src/resources/mapping_continents.json') as json_file:
+            cont_map = json.load(json_file)
+
+        self.continent_maps = cont_map
         self.country_maps = mapping
 
     def join_gdp(self, gdp, olympic):
@@ -18,3 +29,25 @@ class Cleaner():
         joined = pd.merge(temp, teams, how='inner', left_on='tempName', right_on='NOC')
         joined = joined.drop(columns=["tempName"])
         return joined
+    
+    def get_continents_map(self, name):
+        try:
+            country_code = pc.country_name_to_country_alpha2(name, cn_name_format="default")
+            continent_name = pc.country_alpha2_to_continent_code(country_code)
+            return continent_name
+        except:
+            if name == 'ROC' or name == "Republic of Korea" or name == "Chinese Taipei" or name == "Hong Kong, China":
+                return "AS"
+            else:
+                return "Not Available"
+
+    def get_full_continent_map(self, code):
+        return self.continent_maps[code]
+
+    def convert_continent(self, dataframe):
+        continents = dataframe["NOC"].map(lambda x: self.get_continents_map(x)).map(lambda x: self.get_full_continent_map(x))
+        new_final = dataframe.copy()
+        new_final["Continents"] = continents
+        return new_final
+
+
